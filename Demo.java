e882ef83c2cdf41396b814a6f342513dcc1ec02a
nikhil <nikhilgod11@gmail.com>
2022-09-15 15:45:18 +0530

    adde comment

--- Demo.java
+++ Demo.java
@@ -5,7 +5,7 @@ public static void main(String[] args){
       System.out.print.ln("Welcome git");
       System.out.print.ln("Hii...");
       System.out.print.ln("How are you");    
-
+// this file is modified by nikhil godse
       
 }
 }
